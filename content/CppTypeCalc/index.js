a92356cb4f27ea2df0f755e451af39d16d5f90a6
Dmitriy <super.dima771@yandex.ru>
2024-05-05 11:04:18 +0300

    fixed calc, better main page

--- content/CppTypeCalc/index.js
+++ content/CppTypeCalc/index.js
@@ -4,52 +4,76 @@ $(document).ready(function () {
         {
             byte: 1,
             datatype: datatypes.CHAR,
-            val: "signed char"
+            val: "signed char",
+            max_val: 127,
+            min_val: -128
         },{
             byte: 1,
             datatype: datatypes.CHAR,
-            val: "unsigned char"
+            val: "unsigned char",
+            max_val: 255,
+            min_val: 0
         },{
             byte: 1,
             datatype: datatypes.CHAR,
-            val: "Char"
+            val: "Char",
+            max_val: 127,
+            min_val: -128
         },{
             byte: 2,
             datatype: datatypes.NUMBER,
-            val: "short"
+            val: "short",
+            max_val: 32767,
+            min_val: -32768
         },{
             byte: 2,
             datatype: datatypes.NUMBER,
-            val: "unsigned short"
+            val: "unsigned short",
+            max_val: 65535,
+            min_val: 0
         },{
             byte: 2,
             datatype: datatypes.NUMBER,
-            val: "int"
+            val: "int",
+            max_val: 32767,
+            min_val: -32768
         },{
             byte: 2,
             datatype: datatypes.NUMBER,
-            val: "unsigned int"
+            val: "unsigned int",
+            max_val: 65535,
+            min_val: 0
         },{
             byte: 4,
             datatype: datatypes.NUMBER,
-            val: "long"
+            val: "long",
+            max_val: 9223372036854775807,
+            min_val: -9223372036854775808
         },{
             byte: 4,
             datatype: datatypes.NUMBER,
-            val: "unsigned long"
+            val: "unsigned long",
+            max_val: 4294967295,
+            min_val: 0
         },{
             byte: 8,
             datatype: datatypes.NUMBER,
-            val: "long long"
+            val: "long long",
+            max_val: 9223372036854775807,
+            min_val: -9223372036854775808
         },{
             byte: 8,
             datatype: datatypes.NUMBER,
-            val: "unsigned long long"
+            val: "unsigned long long",
+            max_val: 18446744073709551615,
+            min_val: 0
         }
     ]
     $("#btn").click(function () {
         var B = $("#bytes").val()
         var sel = $("#sel").val()
+        var storeto = $("#StoreTo").val()
+        var storeup = $("#StoreUp").val()
         if (sel == "Nothing selected")
             {
                 $("#sel").addClass("is-invalid")
@@ -59,7 +83,7 @@ $(document).ready(function () {
             }
         $("#answer").html("Nothing was found")
         CppDataTypes.forEach((element) => {
-            if (element.byte <= Number(B) && sel.toString() == element.datatype){
+            if (element.byte <= Number(B) && sel.toString() == element.datatype && Number(storeto) <= element.max_val && Number(storeup) >= element.min_val){
                 $("#answer").html(element.val)
             }
         });
--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var proj_url = "https://api.github.com/repos/MaserPlay/js_site/contents/content";
 $.getJSON( proj_url, function( data ) {
-    console.log(data)
     if (!data){
-        return
+        $('#list').html("<section class=\"jumbotron text-center\"> <div class=\"container\"> <h1>Its great time, to start coding... <a href=\"https://github.com/MaserPlay\" class=\"link-offset-2 link-underline link-underline-opacity-0 text-primary\">MaserPlay</a></h1> </div> </section>");
     }
     var finish = ""
     $.each(data, function(d, a){
-        console.log(a)
-        finish = finish + "<div class=\"col-md-4\"> <a class=\"link-offset-2 link-underline link-underline-opacity-0\" href=\"/" + a.path + "\"> <div class=\"card mb-4 shadow-sm\"> <h2 class=\"card-title\"> " + a.name + " </h2> </div> </a> </div> ";
+        $('#list').html(finish + "<div class=\"col-md-4\"> <a class=\"link-offset-2 link-underline link-underline-opacity-0 btn btn-warning\" id=\"elem\" style=\"opacity: 0;\" href=\"/" + a.path + "\"> <div class=\"mb-4\"> <h2  > " + a.name + " </h2> </div> </a> </div> ");
+        $('#elem').fadeTo( 800 , 1)
     });
-    $('#list').html(finish);
   }).fail(function(d) {
     $('#list').html("<section class=\"jumbotron text-center\"> <div class=\"container\"> <h1>Its time, to fix <span class=\"text-danger\"> errors</span>... <a href=\"https://github.com/MaserPlay\" class=\"link-offset-2 link-underline link-underline-opacity-0 text-primary\">MaserPlay</a></h1> </div> </section>");
 });
