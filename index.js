e92c55fade130987d1bb26da2df0e1e4de804bd9
MaserPlay <88372261+MaserPlay@users.noreply.github.com>
2024-06-07 12:39:24 +0000

    Fixed index

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ $.getJSON( proj_url, function( data ) {
         $('#list').html("<section class=\"jumbotron text-center\"> <div class=\"container\"> <h1>Its great time, to start coding... <a href=\"https://github.com/MaserPlay\" class=\"link-offset-2 link-underline link-underline-opacity-0 text-primary\">MaserPlay</a></h1> </div> </section>");
     }
     var finish = ""
-    $.each(data, function(d, a){
-        $('#list').html(finish + "<div class=\"col-md-4\"> <a class=\"link-offset-2 link-underline link-underline-opacity-0 btn btn-warning\" id=\"elem\" style=\"opacity: 0;\" href=\"/" + a.path + "\"> <div class=\"mb-4\"> <h2  > " + a.name + " </h2> </div> </a> </div> ");
+    $.each(data, function(_d, a){
+        finish += "<div class=\"col-md-4\"> <a class=\"link-offset-2 link-underline link-underline-opacity-0 btn btn-warning\" id=\"elem\" style=\"opacity: 0;\" href=\"/" + a.path + "\"> <div class=\"mb-4\"> <h2  > " + a.name + " </h2> </div> </a> </div> ";
+        $('#list').html(finish);
         $('#elem').fadeTo( 800 , 1)
     });
   }).fail(function(d) {
