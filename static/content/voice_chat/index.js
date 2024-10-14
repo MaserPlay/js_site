39ee54dec7a381cbafe2649117c8188fd54ff8c9
Dmitriy <super.dima771@yandex.ru>
2024-10-14 17:55:55 +0300

    fixes

--- static/content/voice_chat/index.js
+++ static/content/voice_chat/index.js
@@ -8,7 +8,8 @@ const userStatus = {
     record_length: 500,
     connect_notification: false,
     disconnect_notification: true,
-    mic: ""
+    mic: "",
+    mic_disconnect_notification: true
   }
   if (localStorage.getItem("username") == null)
   {
@@ -22,6 +23,7 @@ const userStatus = {
   let settings_modal = new bootstrap.Modal('#settings_toast_div');
   var disconnected_toast = new bootstrap.Toast('#disconnected_toast');
   var disconnected_notification = {close: ()=>{}};
+  var mic_disconnected_notification = {close: ()=>{}};
   var connected_notification = {close: ()=>{}};
   
   const usernameInput = $("#username");
@@ -47,10 +49,9 @@ const userStatus = {
     disconnected_toast.show();settings.disconnect_notification&&(disconnected_notification = new Notification("js.maserplay.ru", { body: `Disconected. ${reason.capitalize()}`, icon: "/favicon.ico" }));connected_notification.close();
   });
   
-  
-  function mainFunction() {
-  
+  function getmic(){
     navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      MicAvailable(true)
       if ($('#microphone_select').html().trim() === "")
       {stream.getTracks().forEach((track)=>{
         $('#microphone_select').append($('<option>', {
@@ -59,6 +60,13 @@ const userStatus = {
             disabled: track.muted
         }));
       })}
+    }).catch((e)=>{if (e.message == "Requested device not found"){MicAvailable(false)}; console.error(e)})
+  }
+  
+  function mainFunction() {
+  
+    navigator.mediaDevices.getUserMedia({ audio: true }).then((stream) => {
+      MicAvailable(true)
       if (settings.mic&&settings.mic.trim() !== "")
       {
         stream.addTrack(stream.getTrackById(settings.mic))
@@ -100,6 +108,7 @@ const userStatus = {
         madiaRecorder.stop();
       }, settings.record_length);
     }).catch((err) => {
+      if (e.message == "Requested device not found"){MicAvailable(false)}
       console.error(err);                           // will show "foo"
   }); 
   
@@ -127,31 +136,30 @@ const userStatus = {
     emitUserInformation();
   }
   
-  function toggleConnection(e) {
-    userStatus.online = !userStatus.online;
+  function ChangeConnection(conn) {
+    userStatus.online = conn;
   
-    editButtonClass(e, userStatus.online);
+    editButtonClass($("#onl_btn"), userStatus.online);
     emitUserInformation();
     mainFunction();
   }
   
-  function toggleMute(e) {
-    userStatus.mute = !userStatus.mute;
+  function ChangeMute(mute) {
+    userStatus.mute = mute;
     if (userStatus.mute){madiaRecorder&&madiaRecorder.stop()}
   
-    editButtonClass(e, userStatus.mute);
+    editButtonClass($("#mute_btn"), userStatus.mute);
     emitUserInformation();
   }
   
   
   function editButtonClass(target, bool) {
-    const classList = target.classList;
-    classList.remove("active");
+    target.removeClass("active");
   
     if (bool)
-      return classList.add("active");
+      return target.addClass("active");
   
-    classList.remove("active");
+    target.removeClass("active");
   }
   
   function emitUserInformation() {
@@ -170,13 +178,31 @@ const userStatus = {
   function usersReset(){
     usersDiv.html("")
   }
+  function MicAvailable(isavailable) {
+    if (settings.mic_disconnect_notification)
+    {
+      if (!isavailable)
+      {
+        mic_disconnected_notification = new Notification("js.maserplay.ru", { body: `Microphone disconnected.`, icon: "/favicon.ico" })
+      }
+      else {
+        mic_disconnected_notification.close()
+      }
+      
+    }
+    if (!isavailable)
+    {ChangeMute(true);}
+    $("#mute_btn").attr("disabled", !isavailable);
+  }
   (()=>{
     $("#rec_length_num").val(settings.record_length)
     $("#rec_length").val(settings.record_length)
     $("#disconnect_Notifications_check").prop('checked', settings.disconnect_notification)
     $("#Connect_Notifications_check").prop('checked', settings.connect_notification)
+    $("#mic_disconnect_Notifications_check").prop('checked', settings.mic_disconnect_notification)
   })();
   function saveSettings(){
+    settings.mic_disconnect_notification = $("#mic_disconnect_Notifications_check").prop('checked')
     settings.connect_notification = $("#Connect_Notifications_check").prop('checked')
     settings.disconnect_notification = $("#disconnect_Notifications_check").prop('checked')
     settings.record_length = $("#rec_length_num").val();
