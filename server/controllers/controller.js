b0dc80a67354e39ce6721c7da59009add1d0be6b
Dmitriy <super.dima771@yandex.ru>
2024-10-27 16:37:26 +0300

    better idnex page

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -11,18 +11,24 @@ app.use(index.i18n.init)
 app.use(require("../middleware").main)
 app.use(require("compression")())
 
-app.get("/" , (req , res)=>{
-        res.render("index", {"content": fs.readdirSync("./views/content").map(dirent => dirent), "name": res.__("Index"), "description": res.__("Things in Javascript")});
+app.get("/", (req, res) => {
+    res.render("index", { 
+        "eachcontent": fs.readdirSync("./views/content").map(dirent => ({id: dirent, name: res.__(dirent)})), 
+        "name": res.__("Index"), 
+        "description": res.__("Things in Javascript") 
     });
-app.get('/content/*', (req,res, next) => {
-    if (fs.existsSync("./views/content/" + req.params[0]))
-    {
-        res.render("content/" + req.params[0], {"name": res.__(req.params[0].replace("/", "")), "description": res.__(`${req.params[0].replace("/", "")} description`)})
+});
+app.get('/content/*', (req, res, next) => {
+    if (fs.existsSync("./views/content/" + req.params[0])) {
+        res.render("content/" + req.params[0], {
+            "name": res.__(req.params[0].replace("/", "")), 
+            "description": res.__(`${req.params[0].replace("/", "")} description`)
+        })
         return
     }
     next()
-   })
+})
 
-app.use(function(req,res){
-    res.status(404).render("404", {"name": "404"});
+app.use(function (req, res) {
+    res.status(404).render("404", { "name": "404" });
 });
