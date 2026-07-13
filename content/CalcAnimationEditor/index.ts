ddadce362d374a1939e2ed53bbd9c763c381e31f
MaserPlay <admin@maserplay.ru>
2026-07-14 00:05:24 +0300

    calcanim now works at dark theme!

--- content/CalcAnimationEditor/index.ts
+++ content/CalcAnimationEditor/index.ts
@@ -6,7 +6,6 @@
     const time_show = document.getElementById("time")!;
     
     const objHtmlList = $(".elements-list");
-    setCanvasSize(); // применить сразу при загрузке
 
     var objects : string[] = []
     var newId = 0
@@ -14,6 +13,7 @@
     var offestX = 0
     var offestY = 0
     var tOffset = 0
+    var errorObject = false
 
     function loop() {
         context.clearRect(0,0,canvas.width, canvas.height)
@@ -21,16 +21,20 @@
         const time = (performance.now() - tOffset) / 1000;
         time_show.textContent = "t=" + time.toFixed(4)
         for (const obj of objects) {
+            errorObject = false
             context.beginPath();
             const x = getAndShowErrorIdNeed((obj + "-x"), time)
             const y = getAndShowErrorIdNeed((obj + "-y"), time)
             const scaleE = getAndShowErrorIdNeed((obj + "-scale"), time)
+            context.fillStyle = getValue(obj + "-color")
+            if (errorObject) {
+                continue
+            }
             context.arc(
                 x * scale + canvas.width / 2 + offestX, 
                 y * scale + canvas.height / 2 + offestY, 
                 Math.abs(scaleE * scale), 
                 0, 2 * Math.PI, false);
-            context.fillStyle = getValue(obj + "-color")
             context.fill();
         }
         requestAnimationFrame(loop)
@@ -65,9 +69,9 @@
 
         scale *= Math.exp(-e.deltaY * zoomSpeed);
     })
-    let lastX = 0;
-    let lastY = 0;
-    let dragging = false;
+    var lastX = 0;
+    var lastY = 0;
+    var dragging = false;
 
     canvas.addEventListener("pointerdown", (e) => {
         dragging = true;
@@ -105,28 +109,39 @@
     __("Content/CalcAnimationEditor/Formula/X")
     __("Content/CalcAnimationEditor/Formula/Y")
     __("Content/CalcAnimationEditor/Formula/Scale")
+    const rootStyles = getComputedStyle(document.documentElement);
+    var colorValue = rootStyles.getPropertyValue('--bs-secondary-color').trim();
+    function toHex(colorStr: string) {
+        const ctx = document.createElement('canvas').getContext('2d')!;
+        ctx.fillStyle = colorStr;
+        return ctx.fillStyle;
+    }
     async function genGameObjectHtml(idNamePrefix: string) {
+
+        var inputs = [
+            [__("Content/CalcAnimationEditor/Formula/X"), "x"],
+            [__("Content/CalcAnimationEditor/Formula/Y"), "y"],
+            [__("Content/CalcAnimationEditor/Formula/Scale"), "scale"]
+        ]
+
+        inputs = await Promise.all(inputs.map(async v => ([await v[0],v[1]])))
+
+        const outInputsHtml = 
+        (inputs.map(v => 
+            `<div>
+                <input type="text" class="form-control flex-grow-1" placeholder="${v[0]}" id="${idNamePrefix}-${v[1]}">
+                <div class="invalid-feedback" id="${idNamePrefix}-${v[1]}-error"></div>
+            </div>`
+        )).join("")
         return `<div class="card w-100" id="${idNamePrefix}-div">
                     <div class="card-body">
                         <h5 class="card-title d-flex align-items-center gap-2">
-                            <span class="badge"><input type="color" id="${idNamePrefix}-color" style="width: 1.5rem; height: 1.5rem;"></span>
+                            <span class="badge"><input type="color" value="${colorValue}" id="${idNamePrefix}-color" style="width: 1.5rem; height: 1.5rem;"></span>
                             <div class="evals">
-                                <div>
-                                <input type="text" class="form-control flex-grow-1" placeholder="${await __("Content/CalcAnimationEditor/Formula/X")}" id="${idNamePrefix}-x">
-                                <div class="invalid-feedback" id="${idNamePrefix}-x-error"></div>
-                                </div>
-                                <div>
-                                <input type="text" class="form-control flex-grow-1" placeholder="${await __("Content/CalcAnimationEditor/Formula/Y")}" id="${idNamePrefix}-y">
-                                <div class="invalid-feedback" id="${idNamePrefix}-y-error"></div>
-                                </div>
-                                <div>
-                                <input type="text" class="form-control flex-grow-1" placeholder="${await __("Content/CalcAnimationEditor/Formula/Scale")}" id="${idNamePrefix}-scale">
-                                <div class="invalid-feedback" id="${idNamePrefix}-scale-error"></div>
-                                </div>
+                                ${outInputsHtml}
                             </div>
                         </h5>
-                        <button type="button" class="btn btn-secondary bi bi-trash3" id="${idNamePrefix}-trash">
-                        </button>
+                        <button type="button" class="btn btn-secondary bi bi-trash3" id="${idNamePrefix}-trash"/>
                     </div>
                 </div>`
     }
@@ -149,6 +164,7 @@
         try {
             return runEvalNumber(getValue(id_val), time)
         } catch (e:unknown) {
+            errorObject = true;
             const error = e instanceof Error ? e.message : String(e);
             document.getElementById(id_val)?.classList.add("is-invalid")
             document.getElementById(id_val + "-error")!.textContent = error
@@ -157,5 +173,6 @@
         }
     }
 
+    setCanvasSize(); // применить сразу при загрузке
     setupFullscreen("#fullscreen", "#exit_fullscreen", "#fullscreen_div")
 })()
